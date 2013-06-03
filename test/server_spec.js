8e9c28e45c8064e5c4558baddddcf8b6cd63ae58
David Halls <dahalls@gmail.com>
2013-06-03 08:09:32 +0100

    Pass packet and client to ascoltatore in mosca property of options when publishing. Don't use published messageId when sending packet to client since it's unique to the connection.

--- test/server_spec.js
+++ test/server_spec.js
@@ -709,35 +709,6 @@ describe("mosca.Server", function() {
     });
   });
 
-  it("should receive published messageId", function(done) {
-    buildAndConnect(done, function(client) {
-
-      client.once("publish", function(packet) {
-        expect(packet.messageId).to.be.equal(24);
-        client.disconnect();
-      });
-
-      client.on("suback", function(packet) {
-        client.publish({
-          topic: "hello",
-          qos: 1,
-          messageId: 24
-        });
-      });
-
-      var subscriptions = [{
-          topic: "hello",
-          qos: 1
-        }
-      ];
-
-      client.subscribe({
-        subscriptions: subscriptions,
-        messageId: 42
-      });
-    });
-  });
-
   it("should receive all messages at QoS 0 if a subscription is done with QoS 0", function(done) {
     buildAndConnect(done, function(client) {
 
@@ -891,7 +862,7 @@ describe("mosca.Server", function() {
     ]);
   });
 
-  it("should pass username to backend when publishing", function(done) {
+  it("should pass mosca options to backend when publishing", function(done) {
     instance.authenticate = function(client, username, password, callback) {
       expect(username).to.be.eql("matteo");
       expect(password).to.be.eql("collina");
@@ -904,8 +875,8 @@ describe("mosca.Server", function() {
       var messageId = Math.floor(65535 * Math.random());
 
       instance.ascoltatore.subscribe("hello", function (topic, message, options) {
-        expect(options).to.have.property("messageId", messageId);
-        expect(options).to.have.property("user", "matteo");
+        expect(options.mosca.packet).to.have.property("messageId", messageId);
+        expect(options.mosca.client).to.have.property("user", "matteo");
         client.disconnect();
       });
 
