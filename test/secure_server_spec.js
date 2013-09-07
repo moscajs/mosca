3a31106524f1297f18b0cedf4e1683a4526f59b7
Matteo Collina <matteo.collina@gmail.com>
2013-09-07 18:23:48 +0300

    Secure and non-secure servers can now be started in the same instance.

--- test/secure_server_spec.js
+++ test/secure_server_spec.js
@@ -7,17 +7,25 @@ var SECURE_KEY = __dirname + '/secure/tls-key.pem';
 var SECURE_CERT = __dirname + '/secure/tls-cert.pem';
 
 var moscaSettings = function() {
-  return {
-    port: nextPort(),
+  var settings = {
     logger: {
       childOf: globalLogger,
       level: 60
     },
     secure : { 
+      port: nextPort(),
       keyPath: SECURE_KEY,
       certPath: SECURE_CERT
     }
   };
+
+  // this is required to make the original server
+  // test work
+  // TODO refactor abstract test suite to take
+  // the port as a parameter
+  settings.port = settings.secure.port;
+
+  return settings;
 };
 
 var createConnection = function(port, host, callback) {
@@ -78,4 +86,42 @@ var createConnection = function(port, host, callback) {
 
 describe("mosca.Server - Secure Connection", function() {
   abstractServerTests(moscaSettings, createConnection);
+});
+
+describe("mosca.Server - Secure and non-secure Connection", function() {
+  var settings;
+  var instance;
+  var conn;
+
+  afterEach(function(done) {
+    conn.stream.end();
+    conn.on("close", function() {
+      instance.close(done);
+    });
+  });
+
+  it("should not allow non-secure connections", function(done) {
+    settings = moscaSettings();
+    settings.secure.port = nextPort();
+
+    instance = new mosca.Server(settings, function() {
+      conn = mqtt.createConnection(settings.port);
+      conn.on('error', function(err) {
+        done();
+      });
+    });
+  });
+
+  it("should allow non-secure connections", function(done) {
+    settings = moscaSettings();
+    settings.allowNonSecure = true;
+    settings.secure.port = nextPort();
+
+    instance = new mosca.Server(settings, function() {
+      conn = mqtt.createConnection(settings.port);
+      conn.on('connected', function(err) {
+        done();
+      });
+    });
+  });
 });
