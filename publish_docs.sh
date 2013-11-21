627cb54c0e0405d95c1ec124b1fcf1f4abbd65fd
Matteo Collina <matteo.collina@gmail.com>
2013-11-21 16:14:49 +0100

    Using pre-commit and NPM for pre-commit hooks.

---
+++ publish_docs.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+set -e
+rm -rf docs
+./node_modules/.bin/dox-foundation --source lib --target docs --title Mosca
+git stash	
+rm -rf /tmp/mosca-docs
+cp -R docs /tmp/mosca-docs
+git checkout gh-pages
+git pull origin gh-pages
+rm -rf docs
+cp -R /tmp/mosca-docs docs
+git add docs
+git commit -m "Updated docs"
+git push origin
+git checkout master
+git stash apply
