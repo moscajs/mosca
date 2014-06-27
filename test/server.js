e1b513f115c2dd005ce686dc8428cd9e63a189f8
Morgan Cheng <morgan.chengmo@gmail.com>
2014-06-27 08:40:55 +0800

    change test case name

--- test/server.js
+++ test/server.js
@@ -465,7 +465,7 @@ describe("mosca.Server", function() {
       });
     });
 
-    it("should emit stats event whiling publishing system stats", function(done) {
+    it("should emit stats event whene publishing system stats", function(done) {
       var instance = this.instance;
       var patt = /^\$SYS/;
       buildAndConnect(done, instance, function(client1) {
