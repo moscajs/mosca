d350156c63709ff5249f7a4cdbfc612cea69ba9b
Matteo Collina <hello@matteocollina.com>
2016-05-23 11:24:06 +0200

    removed benchmarks and microtime

--- benchmarks/bombing.js
+++
@@ -1,9 +0,0 @@
-var mqtt = require('mqtt');
-var client = mqtt.createClient(1883, "localhost", { clean: true });
-
-function publish() {
-  client.publish("test", "payload");
-  setImmediate(publish);
-}
-
-client.on("connect", publish);
