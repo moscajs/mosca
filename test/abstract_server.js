99d7e06195501472c9b6211179e58de120f9537d
Simon Hailes <btsimonh@googlemail.com>
2017-05-15 13:32:29 +0100

    Add tests for authorizePublish passing 'ignore' and false

--- test/abstract_server.js
+++ test/abstract_server.js
@@ -852,6 +852,110 @@ module.exports = function(moscaSettings, createConnection) {
     });
   });
 
+  
+  // tests for local authorizePublish, with 'ignore' return
+  var authorizePublishIgnore = function(client, topic, payload, callback) {
+    var auth = true;
+    if (topic === 'authignore'){
+        auth = 'ignore';
+    }
+    if (topic === 'authfalse'){
+        auth = false;
+    }
+    
+    callback(null, auth);
+  }
+  
+  it("should not call onPublished on publish to topic where auth='ignore'", function(done) {
+    var onPublishedCalled = false;
+    var clientId;
+    var count = 0;
+
+    instance.authorizePublish = authorizePublishIgnore;
+    instance.published = function(packet, serverClient, callback) {
+      onPublishedCalled = true;
+      expect(packet.topic).to.be.equal("hello");
+      expect(packet.payload.toString().toString()).to.be.equal("some data");
+      expect(serverClient.id).to.be.equal(clientId);
+
+      callback();
+    };
+
+    buildAndConnect(done, function(client) {
+      clientId = client.opts.clientId;
+
+      client.publish({
+        messageId: 42,
+        topic: "authignore",
+        payload: "some data to ignore",
+        qos: 1
+      });
+      client.publish({
+        messageId: 43,
+        topic: "hello",
+        payload: "some data",
+        qos: 1
+      });
+
+      // auth='ignore' should puback, but not publish
+      client.on("puback", function() {
+        count++
+        // on second call, onPublished should be true
+        if(count === 2){
+            expect(onPublishedCalled).to.eql(true);
+            client.disconnect();
+        }
+      });
+    });
+  });
+  
+  it("should disconnect client on publish to topic where auth=false", function(done) {
+    var onPublishedCalled = false;
+    var clientId;
+    var count = 0;
+    var timer;
+
+    instance.authorizePublish = authorizePublishIgnore;
+    instance.published = function(packet, serverClient, callback) {
+      onPublishedCalled = true;
+      expect(packet.topic).to.be.equal("should not have published");
+      callback();
+    };
+
+    buildAndConnect(done, function(client) {
+      clientId = client.opts.clientId;
+
+      client.publish({
+        messageId: 42,
+        topic: "authfalse",
+        payload: "some data to cause close",
+        qos: 1
+      });
+
+      // if after 2 seconds, we've not closed
+      timer = setTimeout(function(){
+        var test = false;
+        expect(count).to.eql(0);
+        expect(test).to.eql(true);
+        client.disconnect();
+      }, 2000);
+      
+      // auth=false should NOT puback
+      client.on("puback", function() {
+        expect(onPublishedCalled).to.eql(false);
+        count++;
+        expect(count).to.eql(0);
+        client.disconnect();
+      });
+      client.on("close", function() {
+        expect(onPublishedCalled).to.eql(false);
+        expect(count).to.eql(0);
+        client.disconnect();
+        clearTimeout(timer);
+      });
+    });
+  });
+
   it("should emit an event when a new client is connected", function(done) {
     buildClient(done, function(client) {
 
