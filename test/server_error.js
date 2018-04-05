9005cac47039824f990b2d4815ef8ef0d5b1d243
martin-doyle <martin.doyle@dakata.de>
2018-04-05 18:50:09 +0100

    Change server error unit test, add check if err is null

--- test/server_error.js
+++ test/server_error.js
@@ -1,4 +1,5 @@
 var steed = require('steed');
+var assert = chai.assert;
 
 var moscaSettings = function () {
   return {
@@ -46,9 +47,11 @@ describe('mosca.Server.error', function () {
   it('should get MQTT port Error: listen EADDRINUSE', function (done) {
     this.timeout(10000); // have to wait for the inject with delay of two seconds
     instance = new mosca.Server(moscaSettings(), function (err, server) {
+      assert.ifError(err);
       expect(server === instance).to.be.true;
     });
     secondInstance = new mosca.Server(moscaSettings(), function (err, server) {
+      assert.ifError(err);
       expect(server === secondInstance).to.be.true;
     });
     secondInstance.on('error', function (err) {
@@ -59,9 +62,11 @@ describe('mosca.Server.error', function () {
   it('should get HTTP port Error: listen EADDRINUSE', function (done) {
     this.timeout(10000); // have to wait for the inject with delay of two seconds
     instance = new mosca.Server(moscaSettings(), function (err, server) {
+      assert.ifError(err);
       expect(server === instance).to.be.true;
     });
     secondInstance = new mosca.Server(moscaSettings2(), function (err, server) {
+      assert.ifError(err);
       expect(server === secondInstance).to.be.true;
     });
     secondInstance.on('error', function (err) {
